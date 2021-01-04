e228848aa58f29cae9a2564e54c6ef6fc965954c
WalterMitty2112 <devonhope20@gmail.com>
2021-01-04 17:29:21 -0500

    updated

---
+++ console.js
@@ -0,0 +1,13 @@
+//js file for console input/output handeling
+const console = document.getElementById('console');
+
+//set console start output
+console.value = "Welcome to the house of Devon Hopes resume. \n"+
+                "Read the explanations carefully and decide which direction to move in.\n"+
+                "Each direction leads to a new section of the resume.\n\n"+
+                "You are standing in the main entrance way. \n"+
+                "To the North lies the hall of Projects.\n"+
+                "To the East is the office of Work.\n"+
+                "To the West is the den of Education.\n"+
+                "To the South is the library of Memes.\n"+
+                "> ";
