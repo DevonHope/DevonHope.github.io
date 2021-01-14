8ec2655a79a9172abd84c804bbb3017f1d2f31e0
WalterMitty2112 <devonhope20@gmail.com>
2021-01-14 18:25:31 -0500

    update

--- console.js
+++ console.js
@@ -1,18 +1,22 @@
 //js file for console input/output handeling
-const cons = document.getElementById('conout');
-const curarea = document.getElementById('curarea');
 
-//set console start output
-const intro = new String('Welcome to the house of Devon Hopes resume. \n\
-                        Read the explanations carefully and decide which direction to move in.\n\
-                        Each direction leads to a new section of the resume.\n\n\
-                        You are standing in the main entrance way. \n\
-                        To the North lies the hall of Projects.\n\
-                        To the East is the office of Work.\n\
-                        To the West is the den of Education.\n\
-                        To the South is the library of Memes.\n > ');
+//setup process
+function setup(){
+    var cons = document.getElementById('consout');
+    var curarea = document.getElementById('calbl');
+    var inp = document.getElementById('in');
+    //set console start output
+    var intro = new String('Welcome to the house of Devon Hopes resume.\
+                            Read the explanations carefully and decide which direction to move in.\
+                            Each direction leads to a new section of the resume.\
+                            You are standing in the main entrance way. \
+                            To the North lies the hall of Projects.\
+                            To the East is the office of Work.\
+                            To the West is the den of Education.\
+                            To the South is the library of Memes.');
 
-cons.value = '> ';
-curarea.innerHTML = 'Home';
-
+    cons.value = intro;
+    curarea.innerHTML = 'HOME';
+    inp.value = '> ';
+}
 //get values enetered
