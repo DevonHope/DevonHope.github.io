494e0ac59afb2cfb240ca37484616dea68fed18b
WalterMitty2112 <devonhope20@gmail.com>
2021-01-14 22:36:47 -0500

    update

--- console.js
+++ console.js
@@ -1,76 +1,8 @@
 //js file for console input/output handeling
 
-//setup process
-function setup(){
-    var cons = document.getElementById('consout');
-    var curarea = document.getElementById('calbl');
-    var arlbl = document.getElementById('arlbl');
-    var inp = document.getElementById('in');
-    //set console start output
-    var intro = new String('Welcome to the house of Devon Hopes resume.\
-                            \n\nRead the explanations carefully and decide which direction to move in. Each direction leads to a new section of the resume.\
-                            \n\nYou are standing in the main entrance way. \
-                            To the North lies the hall of Projects.\
-                            To the East is the office of Work.\
-                            To the West is the den of Education.\
-                            To the South is the library of Memes.\
-                            In your front pocket is a copy of my resume, enter \'read\' to download.\
-                            \n\nUse \'help\' to display commands.');
+var directions = ['north','south','west','east','help','read','look','map'];
 
-    cons.value = intro;
-    curarea.innerHTML = 'HOME';
-    arlbl.innerHTML = '\>';
-    inp.value = '';
-}
-
-//check for return carridge
-function onEnter(e){
-    if(e.which == 13){
-        cmdloop();
-    }
-}
-
-//cmd loop
-function cmdloop(){
-    //get the input value
-    var inp = document.getElementById('in');
-    //get the textare value
-    var cons = document.getElementById('consout');
-
-    //choices
-    var linp = inp.value.toLowerCase();
-    var value = "";
-    //check if command is prefaced
-    if (linp.split(' ')[0] === 'go') {
-        linp = linp.split(' ')[1];
-        value = maindesc(linp);
-    } else if (linp === 'south' || linp == 'north' || linp == 'west' || linp == 'east' || linp == 'read') {
-        value = maindesc(linp);
-    } else if (linp === 'help') {
-        value = help();
-    } else if (linp === 'read') {
-        download();
-    }else {
-        value = 'Command not recognized, use \'help\' to print usable commands.';
-    }
-
-    //add returned values to consout
-    var newcons = cons.value +"\n\n\> "+ linp+"\n\n"+value;
-    cons.value = newcons;
-    //set scroll bar to bottom
-    cons.scrollTop = cons.scrollHeight;
-    inp.value = '';
-}
-
-function download() {
-    var link = document.createElement('a');
-    link.href = url;
-    link.download = 'assets/DevonHope_Resume.pdf';
-    link.dispatchEvent(new MouseEvent('click'));
-}
-
-function help(){
-    var helpstr = 'HELP - Commands\
+var helpstr = 'HELP - Commands\
                     \n\nDirections:\
                     \nDesitinations are determined by your current position. Any direction can be prefaced with the term \'go\'\
                     \n\'south\'\
@@ -78,23 +10,141 @@ function help(){
                     \n\'west\'\
                     \n\'east\'\
                     \n\nCommands:\
+                    \n\'look\': details your current location\
+                    \n\'map\': provides a map of the site\
                     \n\'read\': to download my resume\
                     \n\'help\': to display this help page';
-    return helpstr;
+
+var intro = 'Welcome to the house of Devon Hopes resume.\
+            \n\nRead the explanations carefully and decide which direction to move in. Each direction leads to a new section of the resume.';
+
+var readdisc = 'In your front pocket is a copy of my resume, enter \'read\' to download.';
+var readbool = false;
+
+var helpdisc = 'Use \'help\' to display commands.';
+
+var homelk = 'You are standing in the main foyer of the Home.\
+            To the North lies the hall of Projects.\
+            To the West is the office of Work.\
+            To the East is the den of Education.\
+            To the South is the library of Memes.';
+
+var projlk = '';
+
+var worklk = '';
+
+var edulk = '';
+
+var memelk = '';
+
+var map = 'this will be a map';
+
+//setup process
+function setup(){
+    var cons = document.getElementById('consout');
+    var curarea = document.getElementById('calbl');
+    var arlbl = document.getElementById('arlbl');
+    var inp = document.getElementById('in');
+    //set console start output
+
+    cons.value = intro+"\n\n"+homelk+readdisc+"\n\n"+helpdisc;
+    curarea.innerHTML = 'HOME';
+    arlbl.innerHTML = '>';
+    inp.value = '';
+    document.getElementById('in').focus();
+}
+
+//check for return carridge
+function onEnter(e){
+    if(e.which === 13){
+        cmdloop();
+    }
+}
+
+//cmd loop
+function cmdloop() {
+    //get the input value
+    var inp = document.getElementById('in');
+    //get the textare value
+    var cons = document.getElementById('consout');
+
+    //choices
+    var rval = val(inp);
+    var value = "";
+    if (rval.includes('help')) {
+        value = helpstr;
+    } else if (rval.includes('look')) {
+        console.log('inside look');
+        value = look();
+    } else if (rval.includes('map')) {
+        value = map;
+    }else if (rval.includes('read')) {
+        download();
+        value = 'Downloaded.';
+    } else if (rval !== false) {
+        value = maindesc(rval);
+    } else {
+        value = 'Command not recognized, use \'help\' to print usable commands.';
+    }
+
+    //add returned values to consout
+    var newcons = cons.value +"\n\n> "+ rval+"\n\n"+value;
+    cons.value = newcons;
+    //set scroll bar to bottom
+    cons.scrollTop = cons.scrollHeight;
+    inp.value = '';
+}
+
+//validate input string
+function val(inp) {
+    var linp = inp.value.toLowerCase();
+    linp = linp.replace(/[^0-9a-z]/gi, '');
+    if (directions.some(substring => linp.includes(substring))) {
+        if (linp.includes('go')) {
+            return linp.split(' ')[1];
+        } else {
+            return linp;
+        }
+    } else {
+        return false;
+    }
+}
+
+function look(){
+    var ca = document.getElementById('calbl').value;
+    if (ca === 'HOME') {
+        console.log('inside look()');
+        return homelk;
+    } else if (ca === 'WORK') {
+        return worklk;
+    } else if (ca === 'PROJECTS') {
+        return projlk;
+    } else if (ca === 'EDUCATION') {
+        return edulk;
+    } else if (ca === 'MEMES') {
+        return memelk;
+    }
+}
+
+function download() {
+    var link = document.createElement('a');
+    link.href = 'assets/DevonHope_Resume.pdf';
+    link.download = 'DevonHope_Resume.pdf';
+    link.dispatchEvent(new MouseEvent('click'));
 }
 
 function maindesc(inp){
     var curar = document.getElementById('calbl');
     var val = "";
-    if(curar = 'HOME'){
+    if (curar.innerHTML === 'HOME') {
         val = home(inp);
-    }else if(curar = 'PROJECTS'){
+    } else if (curar.innerHTML === 'PROJECTS') {
         val = project(inp);
-    }else if(curar = 'WORK'){
+    } else if (curar.innerHTML === 'WORK') {
         val = work(inp);
-    }else if(curar = 'EDUCATION'){
+    } else if (curar.innerHTML === 'EDUCATION') {
         val = education(inp);
-    }else if(curar = 'MEMES'){
+    } else if (curar.innerHTML === 'MEMES') {
         val = memes(inp);
     }
     return val;
@@ -102,24 +152,91 @@ function maindesc(inp){
 
 function home(inp){
     var value = "";
-    if(inp == 'north'){
+    var curarea = document.getElementById('calbl');
+    if (inp.includes('north')) {
         value = "You have entered the hall of Projects."
+        curarea.innerHTML = 'PROJECTS';
+    }else if (inp.includes('south')) {
+        value = "You have entered the library of Memes."
+        curarea.innerHTML = 'MEMES';
+    }else if (inp.includes('east')) {
+        value = "You have entered the den of Education."
+        curarea.innerHTML = 'EDUCATION';
+    } else if (inp.includes('west')) {
+        value = "You have entered the office of Work."
+        curarea.innerHTML = 'WORK';
     }
     return value;
 }
 
-function project(inp){
-
+function project(inp) {
+    var value = "";
+    var curarea = document.getElementById('calbl');
+    if (inp.includes('north')) {
+        value = "I too wish to walk through walls."
+    } else if (inp.includes('south')) {
+        value = "You have returned to the foyer of the Home."
+        curarea.innerHTML = 'HOME';
+    } else if (inp.includes('easst')) {
+        value = "You have entered the den of Education."
+        curarea.innerHTML = 'EDUCATION';
+    } else if (inp.includes('west')) {
+        value = "You have entered the office of Work."
+        curarea.innerHTML = 'WORK';
+    }
+    return value;
 }
 
-function work(inp){
-
+function work(inp) {
+    var value = "";
+    var curarea = document.getElementById('calbl');
+    if (inp.includes('north')) {
+        value = "You have entered the hall of Projects."
+        curarea.innerHTML = 'PROJECTS';
+    } else if (inp.includes('south')) {
+        value = "You have entered the library of Memes."
+        curarea.innerHTML = 'MEMES';
+    } else if (inp.includes('east')) {
+        value = "You have returned to the foyer of the Home."
+        curarea.innerHTML = 'HOME';
+    } else if (inp.includes('west')) {
+        value = "That is a wall."
+    }
+    return value;
 }
 
-function education(inp){
-
+function education(inp) {
+    var value = "";
+    var curarea = document.getElementById('calbl');
+    if (inp.includes('north')) {
+        value = "You have entered the hall of Projects."
+        curarea.innerHTML = 'PROJECTS';
+    } else if (inp.includes('south')) {
+        value = "You have entered the library of Memes."
+        curarea.innerHTML = 'MEMES';
+    } else if (inp.includes('east')) {
+        value = "There is a window here, you can see a bird (chirp-chirp)."
+    } else if (inp.includes('west')) {
+        value = "You have returned to the foyer of the Home."
+        curarea.innerHTML = 'HOME';
+    }
+    return value;
 }
 
-function memes(inp){
-
+function memes(inp) {
+    var value = "";
+    var curarea = document.getElementById('calbl');
+    if (inp.includes('north')) {
+        value = "You have returned to the foyer of the Home."
+        curarea.innerHTML = 'HOME';
+    } else if (inp.includes('south')) {
+        value = "You cannot go through walls."
+    } else if (inp.includes('east')) {
+        value = "You have entered the den of Education."
+        curarea.innerHTML = 'EDUCATION';
+    } else if (inp.includes('west')) {
+        value = "You have entered the office of Work."
+        curarea.innerHTML = 'WORK';
+    }
+    return value;
 }
